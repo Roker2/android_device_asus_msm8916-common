a1297e54ef6c1945ad3a4f8c0b3942554100c3c7
markkiths <markkiths@gmail.com>
2019-07-21 21:08:22 +0700

    add Safetynet bypass

--- msm8916.mk
+++ msm8916.mk
@@ -24,6 +24,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 # HWUI and Dalvik VM overrides
 $(call inherit-product, device/asus/msm8916-common/phone-hdpi-2048-dalvik-heap.mk)
 
+# safetynet bypass
+PRODUCT_PACKAGES += \
+    init.safailnet.rc
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
